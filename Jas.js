4a74142cd5d57eb095b4c676fa65b7d5cc1cd108
Neha Anand <70203777+NehaAnand28@users.noreply.github.com>
2023-07-10 17:12:46 +0530

    Create Jas.js

---
+++ Jas.js
@@ -0,0 +1,26 @@
+
+var options = {
+    "elements": {
+      "rectangle": {
+        "borderWidth": 2
+      }
+    },
+    "layout": {
+      "padding": 0
+    },
+    "legend": {
+      "display": true,
+      "labels": {
+        "boxWidth": 16
+      }
+    },
+    "maintainAspectRatio": false,
+    "responsive": true,
+    "scales": {
+      "xAxes": [
+        {
+          "gridLines": {
+            "display": false
+          },
+          "scaleLabel": {
+            "display": true,
