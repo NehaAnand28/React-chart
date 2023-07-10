954a9f7a58560352d79242987131d20d79f05f41
Neha Anand <70203777+NehaAnand28@users.noreply.github.com>
2023-07-10 17:07:02 +0530

    Create Main.js

---
+++ Main.js
@@ -0,0 +1,68 @@
+var data = {
+    "datavar data = {
+    "datasets": [
+      {
+        "backgroundColor": "rgb(156, 39, 176)",
+        "borderColor": "rgb(156, 39, 176)",
+        "fill": false,
+        "data": [
+          10,
+          120,
+          80,
+          134
+        ],
+        "id": "amount",
+        "label": "Purchase amount (USD)",
+				"yAxisID":"left"
+      },
+      {
+        "backgroundColor": "rgb(39, 176, 200)",
+        "borderColor": "rgb(39, 176, 200)",
+        "fill": falssets": [
+      {
+        "backgroundColor": "rgb(156, 39, 176)",
+        "borderColor": "rgb(156, 39, 176)",
+        "fill": false,
+        "data": [
+          10,
+          120,
+          80,
+          134
+        ],
+        "id": "amount",
+        "label": "Purchase amount (USD)",
+				"yAxisID":"left"
+      },
+      {
+        "backgroundColor": "rgb(39, 176, 200)",
+        "borderColor": "rgb(39, 176, 200)",
+        "fill": false,
+        "data": [
+          300,
+          -1200,
+          500,
+          -340
+        ],
+        "id": "amount",
+        "label": "Purchase amount (USD)",
+				"yAxisID":"right"
+
+      }
+    ],
+    "labels": [
+      "2017-01-02",
+      "2017-04-02",
+      "2017-07-02",
+      "2018-01-02"
+    ]
+  };
+var options = {
+    "elements": {
+      "rectangle": {
+        "borderWidth": 2
+      }
+    },
+    "layout": {
+      "padding": 0
+    },
+    "legend": {
